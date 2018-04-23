e1a4ebcf6c2fdd1345f1d1119f4a535975297ae9
dominika <dominikahoszowska@gmail.com>
2018-04-23 23:38:38 +0200

    sprawdzanie nazwy pliku i wczytywanie hasla do pliku bin

--- ObslugaPlikow.c
+++ ObslugaPlikow.c
@@ -1,13 +1,48 @@
 /*Created by Dominika Hoszowska on 17.04.18.*/
 #include "ObslugaPlikow.h"
+#include "stdio.h"
+#define DLUGOSC 15
+
 int sprawdzNazwePilku(char nazwa[],int dlugosc)/* 0-bledna nazwa, 1- txt 0-bin*/
-{/*TODO*/
-    return 0;}
+{
+    int i=0;
+    while(nazwa[i]!='.'&& i<dlugosc)
+    {
+        i++;
+    }
+    if(i!=dlugosc-4)
+    {
+        return 0;
+    }
+    if(nazwa[dlugosc-3]=='t'&& nazwa[dlugosc-2]=='x'&& nazwa[dlugosc-1]=='t')
+    {
+        return 1;
+    }
+    if(nazwa[dlugosc-3]=='u'&& nazwa[dlugosc-2]=='l'&& nazwa[dlugosc-1]=='a')
+    {
+        return 2;
+    }
+    return 0;
+}
+
 void zapisDoPlikuTxt(char nazwa[])
 {
+    printf("TXT\n");
     /*TODO*/
 }
 void zapisDoPlikuBin(char nazwa[])
 {
+    char haslo[DLUGOSC + 1] = "";
+    int sn = 0;
+    int c;
+    while ((c = getchar()) != EOF  && c!=' '&& sn < DLUGOSC) {
+        if (!(sn == 0 && c == '\n')) {
+            if (c == '\n') {
+                break;
+            }
+            haslo[sn] =(char)c;
+            sn++;
+        }
+    }
     /*TODO*/
 }
