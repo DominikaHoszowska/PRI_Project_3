9f926279b8c576a167ca9f36796fe17c85467364
dominika <dominikahoszowska@gmail.com>
2018-04-23 23:21:20 +0200

    dodanie plikow

---
+++ ObslugaPlikow.c
@@ -0,0 +1,13 @@
+/*Created by Dominika Hoszowska on 17.04.18.*/
+#include "ObslugaPlikow.h"
+int sprawdzNazwePilku(char nazwa[],int dlugosc)/* 0-bledna nazwa, 1- txt 0-bin*/
+{/*TODO*/
+    return 0;}
+void zapisDoPlikuTxt(char nazwa[])
+{
+    /*TODO*/
+}
+void zapisDoPlikuBin(char nazwa[])
+{
+    /*TODO*/
+}
---
+++ ObslugaPlikow.h
@@ -0,0 +1,9 @@
+/*Created by Dominika Hoszowska on 17.04.18.*/
+
+#ifndef PRI_PROJECT_3_OBSLUGAPLIKOW_H
+#define PRI_PROJECT_3_OBSLUGAPLIKOW_H
+int sprawdzNazwePilku(char nazwa[],int dlugosc);/* 0-bledna nazwa, 1- txt 0-bin*/
+void zapisDoPlikuTxt(char nazwa[]);
+void zapisDoPlikuBin(char nazwa[]);
+
+#endif /*PRI_PROJECT_3_OBSLUGAPLIKOW_H*/
